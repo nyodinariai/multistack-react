871f2cb1d5cdfd61401ff0cefbddd11cebdc5345
Nathan Liyodi Nariai <nathan.nariai@gmail.com>
2022-08-08 21:41:38 -0300

    Finalizando a Semana-16

--- Semana-16/ediaristas/src/ui/partials/diarias/_detalhes-diaria.styled.tsx
+++ Semana-16/ediaristas/src/ui/partials/diarias/_detalhes-diaria.styled.tsx
@@ -2,25 +2,59 @@ import { Paper, Typography } from "@material-ui/core";
 import { styled } from "@material-ui/core/styles"; 
 // import { } from'@mui/material'; 
 
-export const CardsContainer = styled('div')` 
+export const CardsContainer = styled('div')`
     display: grid;
-    grid: 1fr;
-    margin-bottom: ${({theme}) => theme.spacing(5)};
-    gap: ${({theme}) => theme.spacing(2)}
+    grid-template-columns: 1fr;
+    margin-bottom: ${({ theme }) => theme.spacing(5)};
+    gap: ${({ theme }) => theme.spacing(2)};
 
-    ${({theme}) => theme.breakpoints.up('md')} {
+    ${({ theme }) => theme.breakpoints.up('md')} {
         grid-template-columns: repeat(2, 1fr);
-        grid-template-areas: 
-        'detail detail'
-        'housecleaner client';
-        gap: ${({theme}) => theme.spacing(7)}
+        grid-template-areas:
+            'detail detail'
+            'housecleaner client';
+        gap: ${({ theme }) => theme.spacing(7)};
     }
 `;
 
 export const  JobTitle = styled(Typography)`
-    color: ${({theme}) => theme.palette.primary.main}
-    font-
+    color: ${({theme}) => theme.palette.primary.main};
+    font-weight: bold;
+    margin-bottom: ${({theme}) => theme.spacing(4)};
+    ${({theme}) => theme.breakpoints.down('md')} {
+        display: none;
+    }
 `;
-export const JobsDetails = styled(Paper)`
+export const JobDetails = styled(Paper)`
+    color: ${({ theme }) => theme.palette.text.secondary};
+    ${({ theme }) => theme.breakpoints.up('md')} {
+        grid-area: detail;
+        padding: ${({theme}) => theme.spacing(4)};
+    }
 
+    ${({ theme }) => theme.breakpoints.down('md')} {
+        box-shadow: none;
+        margin-bottom: ${({theme}) => theme.spacing(5)};
+    }
+`;
+
+export const UserTitle = styled(Typography)`
+    color: ${({ theme }) => theme.palette.primary.main};
+    font-weight: bold;
+
+    ${({ theme }) => theme.breakpoints.down('md')} {
+        background-color: ${({theme}) => theme.palette.grey[100]};
+        padding: ${({ theme }) => theme.spacing(3)};
+        margin: ${({theme}) => '0 ' + theme.spacing(-3)};
+    }
+`;
+export const UserCard = styled(Paper)`
+    display: flex;
+    flex-direction: column;
+    gap: ${({ theme }) => theme.spacing()};
+    padding: ${({ theme }) => theme.spacing(4)};
+    ${({ theme }) => theme.breakpoints.down('md')} {
+        padding: 0;
+        box-shadow: none;
+    }
 `;
